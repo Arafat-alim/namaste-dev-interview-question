8356742924afbea9557ecf5844d0bbd2bbbb2273
arafat-alim <arafat97alim@gmail.com>
2025-06-02 18:48:34 +0530

    ✅ Toggle Password: Test cases passed.

--- react/04_toggle_password/toggle-password.jsx
+++ react/04_toggle_password/toggle-password.jsx
@@ -1,25 +1,35 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Eye, EyeOff } from "lucide-react";
 import "./styles.css";
 
 function TogglePassword() {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleToggleHidePassword = () => {
+    setShowPassword((prev) => !prev);
+  };
+
   return (
     <div className="container">
       <h1 className="title">Toggle Password</h1>
       <div className="password-wrapper">
         <input
-          type="text"
+          type={showPassword ? "text" : "password"}
           id="password"
           placeholder="Enter password"
           className="password-input"
           data-testid="password-input"
         />
-        <span className="icon" data-testid="toggle-icon">
-          <EyeOff size={18} />
+        <span
+          className="icon"
+          data-testid="toggle-icon"
+          onClick={handleToggleHidePassword}
+        >
+          {showPassword ? <Eye size={18} /> : <EyeOff size={18} />}
         </span>
       </div>
       <span className="visibility-label" data-testid="visibility-label">
-        Password Hidden
+        {showPassword ? "Password Visible" : "Password Hidden"}
       </span>
     </div>
   );
